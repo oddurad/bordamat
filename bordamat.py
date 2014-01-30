dd4f7551e0b95520c5fc7cd1e199d3619872dd8c
Oddur Aðalgeirsson <oddur@ubuntu.(none)>
2014-01-30 23:33:24 +0000

    Flask config now comes from an (untracked) config object

--- bordamat.py
+++ bordamat.py
@@ -4,16 +4,9 @@ from flask import (Flask, request, session, g, redirect, url_for,
                    abort, render_template, flash)
 from contextlib import closing
 
-# configuration
-DATABASE = '/tmp/bordamat.db'
-DEBUG = True
-SECRET_KEY = 'belgur'
-USERNAME = 'admin'
-PASSWORD = 'default'
-
 # create our little application :)
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object('config')
 
 
 def connect_db():
